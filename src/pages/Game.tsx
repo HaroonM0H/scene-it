a7c4cb4138bb41e533965d3bb5ec9dc696c006af
Haroon <haroo@Lost-Artifact>
2025-07-09 00:20:23 +0100

    testing

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,9 +1,16 @@
 export default function Game() {
     return(
-        
+        //load first image from the database
+        //display it in the center of the screen
         <div>
-            <h1>Game Page</h1>
-            <p>Welcome to the game!</p>
+            <h1>Scene it</h1>
+            <h2 className="text-appear">
+                Guess the movie from a single frame
+            </h2>
+            <div className="game-container">
+                <img src="/path/to/first/image.jpg" alt="Movie Scene" />
+                <p>Guess the movie!</p>
+            </div>
         </div>
     )
 }
